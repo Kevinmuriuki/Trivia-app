0dc77b625ceba32aeda9075eb901ce161cfbcaac
Kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2022-06-25 13:30:56 +0300

    Add tests for delete  question endpoints

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -67,8 +67,30 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'resource not found')
 
-    
+    def test_delete_question(self):
+        question = Question(question='new question', answer='new answer', difficulty=1, category=1)
+        question.insert()
+        question_id = question.id
 
+        res = self.client().delete(f'/questions/{question_id}')
+        data = json.loads(res.data)
+
+        question = Question.query.filter(Question.id == question.id).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], str(question_id))
+        self.assertEqual(question, None)
+
+    def test_422_sent_deleting_non_existing_question(self):
+        res = self.client().delete('/questions/a')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
+    
+    
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
